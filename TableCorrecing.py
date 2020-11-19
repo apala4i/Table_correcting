f060f60c16dd0c19260b7b3251404c178748d378
apala4i <apala4igm@users.noreply.github.com>
2020-11-19 22:05:21 +0300

    сделал декомпозицию функции редактирования таблицы. Создана функция таблицы. Если её реализовать, то программа заработает.

--- TableCorrecing.py
+++ TableCorrecing.py
@@ -1,1 +1,13 @@
-print()
+def table_correcting(name):
+    """
+    main function that corrects given table and make new file with
+    corrected table
+    :param name: given file name
+    :return:
+    """
+    global file_name
+    pass
+
+file_name = input('Write full path to file')
+table_correcting(file_name)
+print(file_name)
